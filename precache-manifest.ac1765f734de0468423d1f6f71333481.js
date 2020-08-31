38b8b1d86ddb339a69f782d26befa37b6aabfe10
Nikki <archiektor@gmail.com>
2020-08-31 12:37:16 +0200

    Updates

---
+++ precache-manifest.ac1765f734de0468423d1f6f71333481.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd371f889ff5708e8d6f38c3d4dbd2ac",
+    "url": "/TvojaRabota/index.html"
+  },
+  {
+    "revision": "8bb2ed48c7533b7228b4",
+    "url": "/TvojaRabota/static/css/main.8b0b8c41.chunk.css"
+  },
+  {
+    "revision": "53e3d5ecf962e5e5dd65",
+    "url": "/TvojaRabota/static/js/2.392385b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TvojaRabota/static/js/2.392385b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bb2ed48c7533b7228b4",
+    "url": "/TvojaRabota/static/js/main.c35ea856.chunk.js"
+  },
+  {
+    "revision": "68c3098f288025bdc8d7",
+    "url": "/TvojaRabota/static/js/runtime-main.13b299af.js"
+  },
+  {
+    "revision": "3ae1fb12e120d7bc7e15f2084822fdae",
+    "url": "/TvojaRabota/static/media/anna.3ae1fb12.jpg"
+  },
+  {
+    "revision": "e074a9536c161112be80285ab504db9e",
+    "url": "/TvojaRabota/static/media/office.e074a953.jpg"
+  },
+  {
+    "revision": "46dc723c0bad2c23eb3f64badc938ba7",
+    "url": "/TvojaRabota/static/media/rsz_3welder.46dc723c.png"
+  },
+  {
+    "revision": "dc2b0da34f815bb4df45986db3feeff1",
+    "url": "/TvojaRabota/static/media/smiley.dc2b0da3.svg"
+  }
+]);
